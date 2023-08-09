c098bc28f214b2de6bcdee5ff9ea79383f149654
ThongTranHCM <thongtran1311@gmail.com>
2023-08-09 08:57:45 +0700

    Fix ScrollOnLoad.

--- ASPNetReact/ASPNetReact/ClientApp/src/components/ScrollOnLoad.js
+++ ASPNetReact/ASPNetReact/ClientApp/src/components/ScrollOnLoad.js
@@ -7,16 +7,17 @@ import { useLocation } from 'react-router-dom';
 const ScrollOnLoad = () => {
     // Extracts pathname property(key) from an object
     const { pathname } = useLocation();
-    let hash = window.location.hash;
-    console.log("loc ".concat(pathname));
 
-    // Automatically scrolls to top whenever pathname changes
     useEffect(() => {
-        if (window.location.hash.length === 0) {
+        const anchor = window.location.hash.slice(1);
+        if (anchor) {
+            const anchorEl = document.getElementById(anchor);
+            if (anchorEl) {
+                anchorEl.scrollIntoView();
+            }
+        }
+        else {
             window.scrollTo(0, 0);
-        } else {
-            const element = document.getElementById(window.location.hash);
-            //element.scrollIntoView();
         }
     }, [pathname]);
 }
